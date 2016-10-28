0bd00d46547cca7415b41519c1aa36420589b6a5
Ashwini Beke <beke.ashwini@outlook.com>
2016-10-28 16:30:49 +0530

    third commit

--- git_test/Controllers/HomeController.cs
+++ git_test/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace git_test.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page. Hi From Ash";
+            ViewBag.Message = "Your application description page. Hello";
 
             return View();
         }
